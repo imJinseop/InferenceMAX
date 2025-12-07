7184fea05243d3d34ce1e86cd0dd165bec24f224
Cameron Quilici <cjquilici@gmail.com>
2025-12-07 01:52:51 -0600

    chore: migrate Nebius B200 to new cluster (#296)
    * adding changes for new runners
    
    * explicitly state IB HCA devs
    
    * explicitly state IB HCA devs pt 2
    
    * clean up any stale enroot locks
    
    * specify UCX NET DEVICES as eth0
    
    * add umask 002
    
    * going back to separate hf hub caches

--- runners/launch_b200-nb.sh
+++ runners/launch_b200-nb.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-HF_HUB_CACHE_MOUNT="/root/hf_hub_cache-${USER: -1}/"
+HF_HUB_CACHE_MOUNT="/mnt/data/hf-hub-cache-${USER: -1}/"
 PARTITION="main"
 FRAMEWORK_SUFFIX=$([[ "$FRAMEWORK" == "trt" ]] && printf '_trt' || printf '')
+
+UCX_NET_DEVICES=mlx5_4:1,mlx5_5:1,mlx5_6:1,mlx5_7:1,mlx5_8:1,mlx5_9:1,mlx5_10:1,mlx5_11:1
+
+# Cleanup any stale enroot locks from previous runs
+find /var/cache/enroot-container-images/$UID -type f -name "*.lock" | xargs rm
 
 set -x
 srun --partition=$PARTITION --gres=gpu:$TP --exclusive \
@@ -11,5 +16,5 @@ srun --partition=$PARTITION --gres=gpu:$TP --exclusive \
 --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
 --no-container-mount-home --container-writable \
 --container-workdir=/workspace/ \
---no-container-entrypoint --export=ALL,PORT_OFFSET=${USER: -1} \
+--no-container-entrypoint --export=ALL,PORT_OFFSET=${USER: -1},UCX_NET_DEVICES=$UCX_NET_DEVICES \
 bash benchmarks/${EXP_NAME%%_*}_${PRECISION}_b200${FRAMEWORK_SUFFIX}_slurm.sh
